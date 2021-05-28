5578d55fc99876288b63cf4c3885d7c270934f5e
Stylix58 <lateman-jpeg@outlook.fr>
2021-05-28 11:10:39 +0200

    more 2.1

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import handler
 from guizero import *
 
 def update_text():
-    label2.value = handler.debug.testUrl(name.value)
+    f = handler.debug.testUrl(name.value)
+    label2.value = "url archive: {0}, mod type: {1}, mod id: {2}".format(f[0], f[1], f[2])
 
 app = App(title="Forever Mod Installer")
 label = Text(app, text="Test URL")

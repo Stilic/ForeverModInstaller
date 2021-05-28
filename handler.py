e37f88efee297557e1e6a19981a4e403099a70fb
Stylix58 <lateman-jpeg@outlook.fr>
2021-05-28 11:08:19 +0200

    more 2

--- handler.py
+++ handler.py
@@ -2,16 +2,15 @@ import urllib.request as urlreq
 import urllib.parse as urlparse
 from urllib.parse import parse_qs
 from io import StringIO as strio
-
 class PyProtoHandler(urlreq.BaseHandler):
     def fmi_open(self, req):
         fullUrl = req.get_full_url()
-        filePath = "".join(fullUrl.split("://")[1:])
-        return filePath
+        f = "".join(fullUrl.split("://")[1:])
+        return f.split(",")
 
 opener = urlreq.build_opener(PyProtoHandler())
 urlreq.install_opener(opener)
 
 class debug:
     def testUrl(url):
-        return parse_qs(urlparse.urlparse(url).query)["m"]
+        return urlreq.urlopen(url)

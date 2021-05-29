c8b91fdd99f13c3254e22e8e77126b49325f8ca7
Stylix58 <lateman-jpeg@outlook.fr>
2021-05-29 22:16:57 +0200

    v2.0.1 v2

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
-import urllib.request as urlreq
+import urllib
 import os
 import requests
 from zipfile import ZipFile
 import sys
-import aiohttp
-session = aiohttp.ClientSession()
-session.close()
+from tqdm import tqdm
+from download import download
 
 
-def getModsPath():
-    return os.path.abspath(os.path.dirname(os.getcwd() + "/Mods/"))
+modsPath = os.path.abspath(os.path.dirname(os.getcwd() + "/Mods/"))
 
-
-fullUrl = sys.argv[1]
-f = "".join(fullUrl.split("://")[1:])
-f = f.split(",")
-ft = f[0].split("/")
-f[0] = ft[len(ft) - 1]
+f = sys.argv[1].join(sys.argv[1].split("://")[1:]).split(",")
+f[0] = f[0].split("/")[2]
 
 t = requests.get(
-    "https://api.gamebanana.com/Core/Item/Data?itemtype=Mod&itemid={0}&fields=name".format(f[0])).json
+    "https://api.gamebanana.com/Core/Item/Data?itemtype=Mod&itemid={0}&fields=name".format(f[2])).json()
 
-yn = str(input("Do you want to install {0} ?".format(t()[0]) + "(Y/N) "))
+yn = str(input("Do you want to install {0}?".format(t[0]) + " (Y/N) "))
 
 if yn == "y" or "Y":
-    if not os.path.exists("Mods/cache"):
-        os.makedirs("Mods/cache")
     file = f[0] + ".zip"
-    urlreq.urlretrieve("https://gamebanana.com/dl/" + f[0], os.path.abspath(
-        getModsPath() + os.path.sep + "cache" + os.path.sep + file))
-    with ZipFile(getModsPath() + os.path.sep + "cache" + os.path.sep + file, 'r') as zip:
-        zip.extractall(getModsPath())
-    if os.path.exists("Mods/cache/" + file):
-        os.remove(getModsPath() + os.path.sep +
-                  "cache" + os.path.sep + file)
+    download("https://gamebanana.com/dl/" + f[0], modsPath + "/" + t[0], kind="zip", replace=True)
